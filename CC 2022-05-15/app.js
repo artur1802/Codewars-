328b63c0cd6917afa739a9b325c8e5a11af5206d
Arturo Rincon <artur18_02@hotmail.com>
2022-05-15 23:26:26 -0500

    solution

---
+++ CC 2022-05-15/app.js
@@ -0,0 +1,13 @@
+function filter_list(l) {
+    let a =[]
+    
+    for(let i=0; i<=l.length; i++){
+      if(l[i]*1===l[i]){
+        
+        a.push(l[i])
+    }
+    }
+  
+  return a;
+  
+  }
