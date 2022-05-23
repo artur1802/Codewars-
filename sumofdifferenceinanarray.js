c67f51b25b019464d9d05fe8522dedd197e4dddf
Arturo Rincon <artur18_02@hotmail.com>
2022-05-22 23:04:13 -0500

    solution

---
+++ sumofdifferenceinanarray.js
@@ -0,0 +1,9 @@
+//Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+
+
+
+const sumOfDifferences = arr => arr
+  .sort((a, b) => b - a)
+  .map((a, i) => a - arr[i + 1] || 0)
+  .reduce((a, b) => a + b, 0);
