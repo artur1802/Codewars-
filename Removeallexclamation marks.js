2851cb3fb5a0002f6e205bef560aeb25e5b85525
Arturo Rincon <artur18_02@hotmail.com>
2022-05-23 23:03:05 -0500

    solution

--- Removeallexclamation marks.js
+++ Removeallexclamation marks.js
@@ -1,5 +1,11 @@
-//Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
+function sumFromString(str) {
 
-function remove(s) {
-    return s.replace(/!+/g, '') + '!';
+    var arr = str.split(/\D/g);
+    var res = 0;
+    
+    for(var i = 0; i < arr.length; i++){
+      res += +arr[i];
+    }
+    
+    return res;
   }
